b01a40330847481eb80ed6b31ad877b51488c5b4
Tekop1223 <ertec@live.dk>
2022-10-31 09:38:37 +0100

    made tag called Monsters + added Spawn Count

--- Assets/Script/EnemySpawner.cs
+++ Assets/Script/EnemySpawner.cs
@@ -10,6 +10,8 @@ public class EnemySpawner : MonoBehaviour
     private GameObject Slime;
     [SerializeField]
     private GameObject TurtleShell;
+    [SerializeField]
+    private int spawnCount = 0;
 
     [SerializeField]
     private float slimeSpawnTime = 2f;
@@ -29,5 +31,10 @@ public class EnemySpawner : MonoBehaviour
         //Instantiate laver et nyt som er "monster" og vecter3'en bestemmer hvor den spawner
         GameObject newMonster = Instantiate(monster, new Vector3(Random.Range(-110f, -120f), 0, Random.Range(-20f, -25f)), Quaternion.identity);
         StartCoroutine(spawnEnemy(interval, monster));
+    }
+
+    private void Update()
+    {
+        spawnCount = GameObject.FindGameObjectsWithTag("Monsters").Length;
     }
 }
