b48df8a488375a382cf7e13c9a64bd5500538994
Tekop1223 <ertec@live.dk>
2022-11-01 13:48:50 +0100

    added script called Item

---
+++ Assets/Script/Item.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : ScriptableObject
+{
+    [Header("item Information")]
+    public Sprite itemIcon;
+    public string itemName;
+}
