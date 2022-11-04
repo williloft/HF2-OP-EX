7145382a5a2e1acea105bcf18c5fda84b5f61a41
Tekop1223 <ertec@live.dk>
2022-11-04 08:35:51 +0100

    added scriptableObject

--- Assets/Script/Item.cs
+++ Assets/Script/Item.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Item", menuName = "Inventory/Item")]
 public class Item : ScriptableObject
 {
     [Header("item Information")]
     public Sprite itemIcon;
     public string itemName;
+    public GameObject modelPrefab;
 }
--- Assets/Script/WeaponItem.cs
+++ Assets/Script/WeaponItem.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WeaponItem : MonoBehaviour
+public class WeaponItem : Item
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject modelPrefab;
+    public bool isUnarmed;
 }
