1423c805e2a91661c414d6e07367b328d495cf0c
Tekop1223 <ertec@live.dk>
2022-11-04 09:08:57 +0100

    fix mistake with Weapon item & item scripts

--- Assets/Script/WeaponItem.cs
+++ Assets/Script/WeaponItem.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Inventory/item")]
 public class WeaponItem : Item
 {
     public GameObject modelPrefab;
