af9e3047eef7608776c1f68371e326b0a06a2688
Tekop1223 <ertec@live.dk>
2022-11-04 10:00:55 +0100

    added script called WeaponHolderSlot

--- Assets/Script/WeaponHolderSlot.cs
+++ Assets/Script/WeaponHolderSlot.cs
@@ -1,8 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class WeaponHolderSlot : MonoBehaviour
 {
     public Transform parentOverride;
+
+    //tjekker hvilken hånd sværdet skal være i
+    public bool isLeftHandSlot;
+    public bool isRightHandSlot;
+
+    public GameObject currentWeaponModel;
+
+    public void UnloadWeapon()
+    {
+        if(currentWeaponModel != null)
+        {
+            currentWeaponModel.SetActive(false);
+        }
+    }
+
+    public void UnloadWeaponAndDestroy()
+    {
+        if(currentWeaponModel != null)
+        {
+            Destroy(currentWeaponModel);
+        }
+    }
+
+    public void LoadWeaponModel(WeaponItem weaponItem)
+    {
+        UnloadWeaponAndDestroy();
+
+        if (weaponItem == null)
+        {
+            //Unload våben model
+            UnloadWeapon();
+            return; 
+        }
+
+        GameObject model = Instantiate(weaponItem.modelPrefab) as GameObject; //henter våbens prefab i et GameObject kaldet "model"
+        {
+            if (model != null)
+            {
+                if(parentOverride != null)
+                {
+                    model.transform.parent = parentOverride;
+                }
+                else
+                {
+                    model.transform.parent = transform; // det her gør vi for at være sikker på at model har samme transform som scripet
+                }
+
+                model.transform.localPosition = Vector3.zero;
+                model.transform.localRotation = Quaternion.identity;
+                model.transform.localScale = Vector3.one;   
+            }
+
+            currentWeaponModel = model;
+        }
+    }
 }
